155e41f1dfedc62a954fd636f22213ebf1361cbe
CobraLover <6716@holbertonstudents.com>
2024-05-10 16:38:45 +0200

    Task.0-10

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -2,28 +2,14 @@ import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    // call constructor of super class (Building)
     super(brand, motor, color);
-
-    // Create objs
     this._range = range;
   }
 
-  // Methods
-
   cloneCar() {
-    const NewEVCar = this.constructor[Symbol.species] || this.constructor;
-    const clone = new Car;
+    const clone = new (
+      Object.getPrototypeOf(this.constructor))(this._brand, this._motor, this._color);
+    clone._range = this._range;
     return clone;
   }
-
-  // Setters
-
-  // Getters
 }
-/* class TestCar extends Car {};
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
-console.log(tc1 instanceof TestCar);
-console.log(tc2 instanceof TestCar);
-console.log(tc1 == tc2); */
